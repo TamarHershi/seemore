1832f4700b096da3322e898f6511db12bb7753c2
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-07 11:59:24 -0800

    searching from vimeo API sort of working

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,7 @@ class SearchesController < ApplicationController
 
   def search
     search_term = params[:search]
-    @results = HTTParty.get("https://api.vimeo.com/users?page=1&per_page=25&query=#{search_term}")
+    @results = HTTParty.get("https://api.vimeo.com/users?page=1&per_page=25&query=#{search_term}", headers: {"Authorization" => "bearer PUT_TOKEN_HERE"})
   end
   
 end
