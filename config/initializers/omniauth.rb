0c91d16604f42032021ec6d4225154fd88afc17c
Sarah <trowbrsa@gmail.com>
2016-01-07 10:52:07 -0800

    Updated Omniauth.rb file with Twitter info

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :twitter, TWITTER_CLIENT_ID, TWITTER_CLIENT_SECRET
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, ENV["TWITTER_CLIENT_ID"], ["TWITTER_CLIENT_SECRET"], scope: "user:email"
 #
 #
 #   #provider :vimeo
-# end
+end
