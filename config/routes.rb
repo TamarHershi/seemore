1532dfd8d464230af6d7ebcf4e6b9677f315b479
Tammy <misstamar@gmail.com>
2016-01-07 10:12:48 -0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'users#show'
+  get 'users#index'
+  get "/auth/:provider/callback", to: "sessions#create"
+  resources :sessions, except: [:update]
+  resources :creators
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
