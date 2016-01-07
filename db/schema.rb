fd6b3113d238ccd61def51a1843d50e997ab7bc0
Sarah <trowbrsa@gmail.com>
2016-01-07 14:43:35 -0800

    Generated a migration to add avatar url to users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107001244) do
+ActiveRecord::Schema.define(version: 20160107224005) do
 
   create_table "creators", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160107001244) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "avatar_url"
   end
 
   create_table "videos", force: :cascade do |t|
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,15 +3,14 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   let(:twitter_user) { User.new(
-    email:    "a@b.com",
-    username: "Ada",
+    name: "Ada",
     uid:      "1234",
     provider: "twitter")
   }
 
   describe "validations" do
     it "is valid" do
-      expect(user).to be_valid
+      expect(twitter_user).to be_valid
     end
   end
 
