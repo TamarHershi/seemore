d35674c32f13e886bd27c897701e8b04afa7fd02
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-07 11:54:12 -0800

    adding oauth version for vimeo

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,8 @@
 class SearchesController < ApplicationController
 
   def search
+    search_term = params[:search]
+    @results = HTTParty.get("https://api.vimeo.com/users?page=1&per_page=25&query=#{search_term}")
   end
   
 end
