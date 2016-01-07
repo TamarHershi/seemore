34db3d9551dd2381f9e8479019b30b0a4b7e4878
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-07 12:17:39 -0800

    ENV variable working in vimeo API search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,8 @@ class SearchesController < ApplicationController
 
   def search
     search_term = params[:search]
-    @results = HTTParty.get("https://api.vimeo.com/users?page=1&per_page=25&query=#{search_term}", headers: {"Authorization" => "bearer PUT_TOKEN_HERE"})
+    env = ENV["VIMEO_ACCESS_TOKEN"]
+    @results = HTTParty.get("https://api.vimeo.com/users?page=1&per_page=25&query=#{search_term}", headers: {"Authorization" => "bearer #{env}"})
   end
   
 end
