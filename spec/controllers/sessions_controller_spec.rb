c6d1003fe2b534375f83df108aa1eb42cffabb82
Sarah <trowbrsa@gmail.com>
2016-01-07 15:03:27 -0800

    Added rspec test to ensure successful login

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
+  describe "GET #create" do
+    context "logs in with Twitter" do
+      context "it is successful" do
+        before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]}
+        it "redirects to root path" do
+          get :create, provider: :twitter
+          expect(subject).to redirect_to root_path
+        end
+        
+        it "creates a user" do
+
+        end
+      end
+
+      context "it fails" do
+
+      end
+    end
+
+
+  end
 end
