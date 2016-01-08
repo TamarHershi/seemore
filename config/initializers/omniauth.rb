039a4ea95e8721ea5af42cea98e527012feb28e3
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-08 13:50:57 -0800

    thank god it's finally working-- dev portal login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer,
-    :fields => [:name, :uid] if Rails.env.development?
+  provider :developer if Rails.env.development?
 
   provider :twitter, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"], scope: "user:email"
 #
