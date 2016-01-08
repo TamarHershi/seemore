e2e1ab36ad5469f33286d8e46197003285f00003
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-08 13:40:10 -0800

    auth portal working, user not yet being created

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer
+  provider :developer,
+    :fields => [:name, :uid] if Rails.env.development?
 
   provider :twitter, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"], scope: "user:email"
 #
