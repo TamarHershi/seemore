e816e7e046317ec49fc91ff1c8ef7e754a3d95ff
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-08 09:39:43 -0800

    developer portal added

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer, 
+    :fields => [:name],
+    :uid => :name
+
   provider :twitter, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"], scope: "user:email"
 #
 #
