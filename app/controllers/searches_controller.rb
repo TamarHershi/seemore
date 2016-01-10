1bf3e426bdb596f767e68c2a48e6f517469c6bc3
Sarah <trowbrsa@gmail.com>
2016-01-09 21:47:41 -0800

    Fixed merge conflict with searches controller - removed non-working Twitter methods

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,39 +5,13 @@ class SearchesController < ApplicationController
   def search
     search_term = params[:search]
     vimeo_access_token = ENV["VIMEO_ACCESS_TOKEN"]
-<<<<<<< HEAD
-    twitter_bearer_token = Twitter::REST::Client.new do |config|
-      config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-      # config.access_token = ENV['TWITTER_ACCESS_TOKEN']
-      # config.access_token_secret = ENV['TWITTER_ACCESS_SECRET_TOKEN']
-    end
-    twitter_access_token = ENV["TWITTER_ACCESS_TOKEN"]
-    twitter_bearer_token = ENV["TWITTER_BEARER_TOKEN"]
-    if params[:client] == "Vimeo"
-      results = HTTParty.get("https://api.vimeo.com/users?page=1&per_page=25&query=#{search_term}&fields=name,bio,pictures",
-      headers: {"Authorization" => "bearer #{vimeo_access_token}", 'Accept' => 'application/json' }, format: :json).parsed_response
-    elsif params[:client] == "Twitter"
-      # HTTParty.post("api.twitter.com")
-      # headers: {"Authorization" => "Basic #{twitter_bearer_token}", "Content-Type:" => "application/x-www-form-urlencoded;charset=UTF-8""}
-      @results = HTTParty.get("https://api.twitter.com/1.1/users/search.json?q=#{search_term}%20API&page=1&count=25",
-      headers: {"Content-Type:" => "application/x-www-form-urlencoded;charset=UTF-8", "Authorization" => "bearer #{twitter_bearer_token}", 'Accept' => 'application/json' }, format: :json).parsed_response
-    end
-    #  if results["total"] == 0
-    #    flash.now[:error] = "No results matched your search."
-    #  else
-    #    @results = results["data"]
-    #  end
-=======
     results = HTTParty.get("https://api.vimeo.com/users?page=1&per_page=25&query=#{search_term}&fields=name,bio,pictures",
      headers: {"Authorization" => "bearer #{vimeo_access_token}", 'Accept' => 'application/json' }, format: :json).parsed_response
      if results["total"] == 0
        flash.now[:error] = "No results matched your search."
      else
        @results = results["data"]
-       @provider = "twitter"
      end
->>>>>>> 820eb8b684226b66c5b73e6c34e30385ff1eca1c
   end
 
 
