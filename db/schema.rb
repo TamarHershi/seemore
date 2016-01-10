c6cdcbd4fbbf2e5d7d7bfd498eac9f4f355b6d7d
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-10 12:58:17 -0800

    removed extraneous col from videos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110203620) do
+ActiveRecord::Schema.define(version: 20160110205724) do
 
   create_table "categories", force: :cascade do |t|
     t.integer  "user_id"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 20160110203620) do
     t.string   "uri"
     t.string   "name"
     t.string   "embed"
-    t.string   "vimeo_id"
     t.string   "description"
   end
 
