a0a936e40d232a1a14444e06dfeb94bb708b3c92
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-11 12:29:02 -0800

    bug fixed so you can't follow someone twice, and throws a different flash mg

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -10,7 +10,7 @@ class CreatorsController < ApplicationController
 
   def create
     # find or create a Creator
-    @creator = Creator.find_by(name: params["uri"])
+    @creator = Creator.find_by(uri: params["uri"])
     if !@creator.nil?
       if !@current_user.creators.include?(@creator)
         @creator.users << @current_user
