699696b8a23c2692095283aa8e57621d1eb3bb18
Tammy <misstamar@gmail.com>
2016-01-11 11:21:46 -0800

    still can't follow vimeo, but fuxed some bugs

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,7 +6,7 @@ class SearchesController < ApplicationController
     if params[:provider] == "vimeo"
       search_term = params[:search]
       vimeo_access_token = ENV["VIMEO_ACCESS_TOKEN"]
-      results = HTTParty.get("https://api.vimeo.com/users?page=1&per_page=25&query=#{search_term}&fields=name,bio,pictures",
+      results = HTTParty.get("https://api.vimeo.com/users?page=1&per_page=25&query=#{search_term}&fields=uri,name,bio,pictures",
        headers: {"Authorization" => "bearer #{vimeo_access_token}", 'Accept' => 'application/json' }, format: :json).parsed_response
        if results["total"] == 0
          flash.now[:error] = "No results matched your search."
