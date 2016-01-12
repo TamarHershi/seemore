ecfaac85ca379001250714e26d4ee317a562a8de
emgord <emgord@gmail.com>
2016-01-11 20:54:15 -0800

    added tests for vimeo authentication for sessions_controller_spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -57,7 +57,64 @@ RSpec.describe SessionsController, type: :controller do
         it "returns flash notice to include 'Failed to authenticate'" do
           get :create, provider: :twitter
           expect(flash[:notice]).to include "Failed to authenticate"
+        end
+      end
+    end
 
+    context "logs in with Vimeo" do
+      context "when it is successful" do
+        before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:vimeo]}
+        it "redirects to feed path" do
+          get :create, provider: :vimeo
+          expect(subject).to redirect_to feed_path
+        end
+        it "creates a user" do
+          expect {get :create, provider: :vimeo}.to change(User, :count).by(1)
+        end
+        it "assigns the @user variable" do
+          get :create, provider: :vimeo
+          expect(assigns(:user)).to be_an_instance_of(User)
+        end
+        it "assigns session[:user_id]" do
+          get :create, provider: :vimeo
+          expect(session[:user_id]).to eq assigns(:user).id
+        end
+      end
+      context "when user has signed up before" do
+        before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:vimeo]}
+        let!(:user) {User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:vimeo])}
+        it "doesn't create another user" do
+          expect {get :create, provider: :vimeo}.to change(User, :count).by(0)
+        end
+        it "assigns the session[:user_id]" do
+          get :create, provider: :vimeo
+          expect(session[:user_id]).to eq user.id
+        end
+        it "assigns the @user variable" do
+          get :create, provider: :vimeo
+          expect(assigns(:user)).to be_an_instance_of(User)
+        end
+      end
+      context "it fails to save the user" do
+        before { request.env["omniauth.auth"] = {"uid" => "not correct", "info" => {}}}
+        it "redirect to root path" do
+          get :create, provider: :vimeo
+          expect(response).to redirect_to root_path
+        end
+        it "returns flash notice to include 'Failed to save the user'" do
+          get :create, provider: :vimeo
+          expect(flash[:notice]).to include "Failed to save the user"
+        end
+      end
+      context "it fails on vimeo" do
+        before { request.env["omniauth.auth"] = "bad login"}
+        it "redirect to root path" do
+          get :create, provider: :vimeo
+          expect(response).to redirect_to root_path
+        end
+        it "returns flash notice to include 'Failed to authenticate'" do
+          get :create, provider: :vimeo
+          expect(flash[:notice]).to include "Failed to authenticate"
         end
       end
     end
