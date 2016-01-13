ddca3066b9bf74797758b42217a5e182caee9520
emgord <emgord@gmail.com>
2016-01-13 11:20:42 -0800

    experimenting with twitter gem to embed tweets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
 
   def show
     @videos = @current_user.videos
+    @tweets = $twitter.user_timeline("sarahinsea").take(25)
+    @tweet = $twitter.oembed('684107967449272321')
   end
 
 end
