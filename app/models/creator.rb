afc75e0e8bce71948ee5fdeef424d15d0fed92aa
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-12 17:41:49 -0800

    moved #create_videe_videos to Video model, updated methods in Creator model

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -10,31 +10,10 @@ class Creator < ActiveRecord::Base
     vimeo_access_token = ENV["VIMEO_ACCESS_TOKEN"]
     videos = HTTParty.get("https://api.vimeo.com/#{self.uri}/videos",
       headers: {"Authorization" => "bearer #{vimeo_access_token}", 'Accept' => 'application/json' }, format: :json).parsed_response
-    return videos
-  end
-
-  def videos?(videos)
-    !videos["data"].nil?
-  end
-
-  def create_videos
-    videos = get_videos_info
-    if videos?
-      videos["data"].each do |video_json|
-        video_id = video_json["uri"].gsub(/[^0-9]/, "")
-        vid = Video.new ({
-        uri: "#{video_json["uri"]}",
-        name: "#{video_json["name"]}",
-        description: "#{video_json["description"]}",
-        embed: "https:\/\/player.vimeo.com\/video\/#{video_id}",
-        posted_at: "#{video_json["created_time"]}",
-        vimeo_id: "#{video_id}"
-      })
-        vid.creator_id = self.id
-        vid.save
-      end
-    else
+    if videos["data"].nil?
       return nil
+    else
+      return videos["data"]
     end
   end
 
--- app/models/video.rb
+++ app/models/video.rb
@@ -3,4 +3,23 @@ class Video < ActiveRecord::Base
   validates :name, :uri, :embed, :vimeo_id, :posted_at,
    presence: true
 
+   def self.create_videos__for_creator_from_hashes(video_data_array, creator)
+     video_data_array.each do |video_hash|
+       video_id = video_hash["uri"].gsub(/[^0-9]/, "")
+       vid = Video.new ({
+       uri: "#{video_hash["uri"]}",
+       name: "#{video_hash["name"]}",
+       description: "#{video_hash["description"]}",
+       embed: "https:\/\/player.vimeo.com\/video\/#{video_id}",
+       posted_at: "#{video_hash["created_time"]}",
+       vimeo_id: "#{video_id}"
+     })
+       vid.creator_id = creator.id
+       vid.save
+     end
+   else
+     return nil
+   end
+   end
+
 end
