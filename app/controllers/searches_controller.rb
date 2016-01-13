2b264465462f62488a7f719801cf22a2b5d49638
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-12 18:59:45 -0800

    removed twitter api call from search method

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,14 +10,14 @@ class SearchesController < ApplicationController
       search_term = params[:search]
       @provider = params[:provider]
       if @provider == "vimeo"
-        results = search_api_call(search_term)
+        results = vimeo_search_api_call(search_term)
          if results["total"] == 0
            flash.now[:error] = "No results matched your search."
          else
            @results = results["data"]
          end
       elsif @provider == "twitter"
-        @results = $twitter.user_search("#{search_term}").take(25)
+        @results = twitter_search_api_call(search_term)
         if @results.length == 0
           flash.now[:error] = "No results matched your search."
         end
@@ -27,11 +27,15 @@ class SearchesController < ApplicationController
 
   private
 
-  def search_api_call(search_term)
+  def vimeo_search_api_call(search_term)
     vimeo_access_token = ENV["VIMEO_ACCESS_TOKEN"]
     results = HTTParty.get("https://api.vimeo.com/users?page=1&per_page=25&query=#{search_term}&fields=uri,name,pictures",
      headers: {"Authorization" => "bearer #{vimeo_access_token}", 'Accept' => 'application/json' }, format: :json).parsed_response
     return results
+  end
+
+  def twitter_search_api_call(search_term)
+    $twitter.user_search("#{search_term}").take(25)
   end
 
 end
