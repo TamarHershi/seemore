0908046644419e09756420bb877dfc062bf50216
Tammy <misstamar@gmail.com>
2016-01-13 11:01:39 -0800

    got the creator model Rspec design

--- spec/models/creator_spec.rb
+++ spec/models/creator_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Creator, type: :model do
 
-  let (:creator1) {FactoryGirl.create(:creator)}
-  let (:user1) { FactoryGirl.create(:user)}
+  let!(:creator1) {FactoryGirl.create(:creator)}
+  let!(:user1) { FactoryGirl.create(:twitter_user)}
 
   let(:log_in) {
   current_user = create :user
@@ -11,15 +11,60 @@ RSpec.describe Creator, type: :model do
   }
 
   describe "validations" do
-    it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_presence_of(:uid) }
-    it "must have a unique uid" do
+    # it { is_expected.to validate_presence_of(:name) }
+    # it { is_expected.to validate_presence_of(:uid) }
+    it "must have a unique name and uid" do
       expect(Creator.new(name: creator1.name, uid: "gooola")).to_not be_valid
       expect(Creator.new(name: "zoxksmwbdjcusadk23053fd")).to be_valid
     end
   end
 
   describe '.find or create' do
+
+    context 'creator exists' do
+      it 'returns that creator' do
+
+      end
+    end
+
+    context 'need to create new Creator' do
+      it 'creates new Creator' do
+      end
+
+      context 'the provider is Twitter' do
+
+        it 'saves the tweets that associated with that creator' do
+        end
+
+        it 'if the tweets are not populated correctly Creator is not saved ' do
+        end
+
+        it 'save the Creator if there are no tweets' do
+        end
+
+        it 'has a profile picture' do
+        end
+
+      end
+
+      context 'the provider is Vimeo' do
+
+        it 'saves the videos associated with that creator' do
+        end
+
+        it 'if the videos are not populated correctly Creator is not saved ' do
+        end
+
+        it 'save the Creator if there are no videos' do
+        end
+
+        it 'has a profile picture' do
+        end
+      end
+
+
+      end
+    end
   end
 
 
@@ -32,6 +77,7 @@ RSpec.describe Creator, type: :model do
       VCR.use_cassette 'get_videos' do
 
     end
+  end
 
 
 end
