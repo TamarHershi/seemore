43f712b40b2929983190df01643340ee72106556
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-12 18:12:39 -0800

    removed search_api_call from main search method

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,9 +9,7 @@ class SearchesController < ApplicationController
     else
       if params[:provider] == "vimeo"
         search_term = params[:search]
-        vimeo_access_token = ENV["VIMEO_ACCESS_TOKEN"]
-        results = HTTParty.get("https://api.vimeo.com/users?page=1&per_page=25&query=#{search_term}&fields=uri,name,bio,pictures",
-         headers: {"Authorization" => "bearer #{vimeo_access_token}", 'Accept' => 'application/json' }, format: :json).parsed_response
+        results = search_api_call(search_term)
          if results["total"] == 0
            flash.now[:error] = "No results matched your search."
          else
@@ -29,4 +27,14 @@ class SearchesController < ApplicationController
       end
     end
   end
+
+  private
+
+  def search_api_call(search_term)
+    vimeo_access_token = ENV["VIMEO_ACCESS_TOKEN"]
+    results = HTTParty.get("https://api.vimeo.com/users?page=1&per_page=25&query=#{search_term}&fields=uri,name,pictures",
+     headers: {"Authorization" => "bearer #{vimeo_access_token}", 'Accept' => 'application/json' }, format: :json).parsed_response
+    return results
+  end
+
 end
