33f7218c3e507a15b0f0fb5cc0836e0f9dbd2d5e
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-14 09:47:07 -0800

    limiting number of videos and tweets shown on feed to most recent 25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
 # get all tweets from User
 # save them to db
   def show
-    @videos = @current_user.videos
-    @tweets = @current_user.tweets
+    @videos = @current_user.videos.to_ary.sort_by{|video| video.posted_at}[0..24]
+    @tweets = @current_user.tweets.to_ary.sort_by{|tweet| tweet.posted_at}[0..24]
+    @media = [@videos, @tweets].flatten.sort_by{|media| media.posted_at}
   end
 
 end
