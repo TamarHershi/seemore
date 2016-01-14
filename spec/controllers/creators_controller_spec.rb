ffae1f0b0313dad573f78bb2f8efc3a7c3f901d5
Sarah <trowbrsa@gmail.com>
2016-01-14 13:46:49 -0800

    more draft creator controller specs

--- spec/controllers/creators_controller_spec.rb
+++ spec/controllers/creators_controller_spec.rb
@@ -3,57 +3,91 @@ require 'spec_helper'
 
 RSpec.describe CreatorsController, type: :controller do
 
-    let(:category) { create(:category) }
-    let(:user) { category.user }
-    let(:creator) { category.creator }
+  describe "GET index" do
 
-    before :each do
-      session[:user_id] = user.id
-    end
+    context "user has not followed any creators" do
+      let(:user) {create(:twitter_user) }
 
-    describe "GET index" do
-
-    context "is successful" do
-      it "renders creator index page" do
-        get :index
-        expect(response).to render_template :index
+      before :each do
+        session[:user_id] = user.id
       end
 
-      it "is successful" do
-        get :index
-        expect(response.status).to eq 200
+# this doesn't work ATM - need to change twitter_user to new build
+      # it "gives a flash notice" do
+      #   get :index
+      #   expect(flash[:notice]).to be_present
+      # end
+    end
+
+    context "user has followed a creator" do
+      let(:category) { create(:category) }
+      let(:user) { category.user }
+      let(:creator) { category.creator }
+
+      before :each do
+        session[:user_id] = user.id
       end
 
-      it "shows creators that the user is following" do
-        get :index
-        expect(assigns(:creators)).not_to be_empty
-        expect(assigns(:creators)[0]).to be_an_instance_of(Creator)
+      context "is successful" do
+        it "renders creator index page" do
+          get :index
+          expect(response).to render_template :index
+        end
+
+        it "is successful" do
+          get :index
+          expect(response.status).to eq 200
+        end
+
+        it "shows creators that the user is following" do
+          get :index
+          expect(assigns(:creators)).not_to be_empty
+          expect(assigns(:creators)[0]).to be_an_instance_of(Creator)
+        end
       end
-    end
-
-  describe "POST create" do
-
-    it "returns an instance of Creator if one already exists for a certain provider user" do
-
-    end
-    it "creates a new instance of Creator to follow if one doesn't exist" do
-
-    end
-    it "saves all the media for a new Creator" do
-
-    end
-    it "does not create duplicate instances Creator with same proivder & uid" do
-
-    end
-    it "does not allow you to follow a Creator you're already following" do
-
-    end
-    it "does not save new instance of Creator unless media is saved as well" do
-
-    end
-    it "sets up the Categories relationship between @current_user and @creator" do
-
     end
   end
 
+  describe "POST create" do
+    context "it finds an existing creator" do
+      let(:user) {create(:twitter_user) }
+
+      before :each do
+        session[:user_id] = user.id
+        request.env["HTTP_REFERER"] = "from_where_I_was"
+      end
+
+      it "returns an instance of Creator if one already exists for a certain provider user" do
+        # add all parameter for params
+        creator = Creator.find_or_create(uid: "1ab3da5")
+        get :follow
+        expect(creator).to be_an_instance_of(Creator)
+        expect(subject).to redirect_to "from_where_I_was"
+      end
+    end
+
+    # it "flashes an error if creator doesn't exist" do
+    #
+    # end
+    #
+    # it "creates a new instance of Creator to follow if one doesn't exist" do
+    #
+    # end
+    #
+    # it "saves all the media for a new Creator" do
+    #
+    # end
+    # it "does not create duplicate instances Creator with same proivder & uid" do
+    #
+    # end
+    # it "does not allow you to follow a Creator you're already following" do
+    #
+    # end
+    # it "does not save new instance of Creator unless media is saved as well" do
+    #
+    # end
+    # it "sets up the Categories relationship between @current_user and @creator" do
+    #
+    # end
+  end
 end
