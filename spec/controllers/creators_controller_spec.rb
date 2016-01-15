89857fcedbe27096f69d3d533227e87228f11f59
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-14 19:20:35 -0800

    DRYed creator controller specs, added test for flash.now

--- spec/controllers/creators_controller_spec.rb
+++ spec/controllers/creators_controller_spec.rb
@@ -4,42 +4,50 @@ require 'pry'
 
 RSpec.describe CreatorsController, type: :controller do
 
-    describe "GET index" do
+  describe "GET index" do
 
-      context "User is successfully following a Creator" do
-        let(:user) {create(:twitter_user) }
+    context "user has followed a creator" do
 
-      context "user has followed a creator" do
-        let(:category) { create(:category) }
-        let(:user) { category.user }
-        let(:creator) { category.creator }
+      let(:category) { create(:category) }
+      let(:user) { category.user }
+      let(:creator) { category.creator }
 
-        before :each do
-          session[:user_id] = user.id
-          request.env["HTTP_REFERER"] = "from_where_I_was"
-        end
+      before :each do
+        session[:user_id] = user.id
+        request.env["HTTP_REFERER"] = "from_where_I_was"
+      end
 
-        context "is successful" do
-          it "renders creator index page" do
-            get :index
-            expect(response).to render_template :index
-          end
+      it "renders creator index page" do
+        get :index
+        expect(response).to render_template :index
+      end
 
-          it "is successful" do
-            get :index
-            expect(response.status).to eq 200
-          end
+      it "is successful" do
+        get :index
+        expect(response.status).to eq 200
+      end
 
-          it "shows creators that the user is following" do
-            get :index
-            expect(assigns(:creators)).not_to be_empty
-            expect(assigns(:creators)[0]).to be_an_instance_of(Creator)
-          end
-        end
+      it "shows creators that the user is following" do
+        get :index
+        expect(assigns(:creators)).not_to be_empty
+        expect(assigns(:creators)[0]).to be_an_instance_of(Creator)
       end
     end
 
-    describe "POST follow" do
+      context "user has not followed anyone" do
+        let (:user) {create(:twitter_user)}
+
+        it "displays a flash.now message" do
+          get :index
+          expect { flash.instance_variable_get(:@now)}.not_to be_nil 
+        end
+
+      end
+  end
+
+  describe "POST #follow" do
+
+    context "it finds an existing creator" do
 
       let(:creator_params) do
         { name: "Edward",
@@ -50,53 +58,26 @@ RSpec.describe CreatorsController, type: :controller do
         }
       end
 
+      let(:user) {create(:twitter_user) }
+
       before :each do
         session[:user_id] = user.id
         request.env["HTTP_REFERER"] = "from_where_I_was"
       end
 
-      context "it finds an existing creator" do
-        let(:user) {create(:twitter_user) }
-        let (:creator) {create(:twitter_creator) }
-
-        it "follows a creator" do
-          get :follow, creator_params
-          expect(flash[:notice]).to include "You're now following #{creator.name}."
-          expect(subject).to redirect_to "from_where_I_was"
-        end
+      it "follows a creator" do
+        get :follow, creator_params
+        expect(flash[:notice]).to include "You're now following #{creator.name}."
+        expect(subject).to redirect_to "from_where_I_was"
       end
-    end
-  end
 
-  describe "GET #index" do
-
-    before :each do
-      session[:user_id] = user.id
-    end
-
-    context "User failed to follow Creator" do
-      let(:user) {create(:twitter_user_not_following) }
-
-      it "gives a flash notice" do
-        get :index
-        expect(response.status).to eq 200
-        expect(flash[:notice]).to include "Try following some people first!"
-      end
-    end
-  end
-
-  describe "POST #follow" do
-
-    let(:category_2) { create(:category_2) }
-    let(:user_2) { category_2.user }
-    let(:creator_2) { category_2.creator }
-
-    before :each do
-      session[:user_id] = user_2.id
-      request.env["HTTP_REFERER"] = "from_where_I_was"
     end
 
     context "creator is already followed" do
+
+      let(:category_2) { create(:category_2) }
+      let(:user_2) { category_2.user }
+      let(:creator_2) { category_2.creator }
 
       let(:creator_params) do
         { name: "Eddie",
@@ -105,6 +86,11 @@ RSpec.describe CreatorsController, type: :controller do
           provider: "twitter",
           uid: "fdsfdas"
         }
+      end
+
+      before :each do
+        session[:user_id] = user_2.id
+        request.env["HTTP_REFERER"] = "from_where_I_was"
       end
 
       it "gives an error" do
@@ -141,5 +127,4 @@ RSpec.describe CreatorsController, type: :controller do
       expect(subject).to redirect_to "from_where_I_was"
     end
   end
-
 end
